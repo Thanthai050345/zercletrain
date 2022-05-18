bd4812925b11ec2da985273edb803b0c507b1b4c
Thanthai <thanthai.j@kkumail.com>
2022-05-18 23:38:54 +0700

    pokemonApi push

--- pogemonApi/src/js/function.js
+++ pogemonApi/src/js/function.js
@@ -1,29 +1,59 @@
+let allPokemon = [];
+
 const getPokemon = async() => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-    const data = await response.json()
-    return data
-}
+    const random = Math.floor(Math.random() * (1026 - 0)) + 0;
+    let loadMore = `https://pokeapi.co/api/v2/pokemon?offset=${random}&limit=20`;
+    const response = await fetch(loadMore);
+    const data = await response.json();
 
-const createEle = async() => {
-    const divContainer = document.getElementById("divContainer")
+    function createPokemonObjects(result) {
+        result.forEach(async(element) => {
+            const res = await fetch(
+                `https://pokeapi.co/api/v2/pokemon/${element.name}`
+            );
+            const data = await res.json();
+            allPokemon.push(data);
+            const urlImage = data.sprites.front_default;
+            const name = data.name;
+            const types = data.types;
+            createElement(urlImage, name, types);
+        });
+    }
+    createPokemonObjects(data.results);
+};
 
-    const cardDiv = document.createElement("div")
-    const pokemonImg = document.createElement("img")
-    const textDiv = document.createElement("div")
-    const title = document.createElement("div")
-    const description = document.createElement("p")
+const createElement = async(imgUrl, name, skill) => {
+    const divContainer = document.getElementById("divContainer");
 
-    cardDiv.setAttribute("class", "rounded overflow-hidden bg-white round")
-    textDiv.setAttribute("class", "px-6 py-4 flex align-center flex-col")
-    title.setAttribute("class", "font-bold text-xl mb-2")
-    description.setAttribute("class", "text-gray-700 text-base")
+    const cardDiv = document.createElement("div");
+    const pokemonImg = document.createElement("img");
+    const textDiv = document.createElement("div");
+    const title = document.createElement("p");
+    const description = document.createElement("p");
 
-    const allPokemon = await getPokemon()
-    console.log(allPokemon);
+    cardDiv.setAttribute("class", "rounded overflow-hidden bg-white round");
+    textDiv.setAttribute("class", "text-center");
+    title.setAttribute("class", "font-bold mb-2 text-center");
+    description.setAttribute("class", "text-gray-400 text-base");
 
-    cardDiv.appendChild(pokemonImg)
-    divContainer.appendChild(cardDiv)
+    pokemonImg.setAttribute("src", imgUrl);
+    pokemonImg.setAttribute("class", "m-auto block w-80");
 
-}
+    title.innerHTML = name;
+    skill.filter((element) => {
+        element.type.name === "glass";
+        description.innerHTML += `${element.type.name} `;
+    });
 
-createEle()
+    textDiv.appendChild(title);
+    textDiv.appendChild(description);
+
+    cardDiv.appendChild(pokemonImg);
+    cardDiv.appendChild(textDiv);
+    // cardDiv.appendChild(title);
+    // cardDiv.appendChild(description);
+
+    divContainer.appendChild(cardDiv);
+};
+
+getPokemon();
